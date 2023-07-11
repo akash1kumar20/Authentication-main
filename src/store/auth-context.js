c472bd21c00980eff8d20436981ebb060480c696
akash1kumar20 <akashkumar2101sm@gmail.com>
2023-07-11 10:55:07 +0530

    auto-logout

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -14,6 +14,7 @@ export const AuthContextProvider = (props) => {
   const loginHandler = (token) => {
     setToken(token);
     localStorage.setItem("token", token);
+    setTimeout(logoutHandler, 50000);
   };
   const logoutHandler = () => {
     setToken(null);
